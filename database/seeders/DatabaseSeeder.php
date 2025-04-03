7168937e88b8ccf7503336318187b1c090b78b24
vladzo <vashchukvlad17@gmail.com>
2025-04-04 01:24:44 +0300

    Seed the job_listing table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +18,28 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+
+
+
+
+        DB::table('job_listing')->insert([
+            'title' => 'Programmer',
+            'salary' => '50.000$',
+        ]);
+
+        DB::table('job_listing')->insert([
+            'title' => 'Manager',
+            'salary' => '60.000$',
+        ]);
+
+        DB::table('job_listing')->insert([
+            'title' => 'Teacher',
+            'salary' => '1000.000$',
         ]);
     }
 }
