4d39f4e056aa88cf5d0eb1a202df4cf176d17e11
vladzo <vashchukvlad17@gmail.com>
2025-04-04 16:17:19 +0300

    Add Employer

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -5,11 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Job extends Model
+class Employer extends Model
 {
     use HasFactory;
-
-    protected $table = 'job_listing';
-    
-    protected $fillable = ['title', 'salary'];
 }
