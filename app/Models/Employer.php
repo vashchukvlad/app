376acfbfc2f5a033aca5229e35298edb01d64330
vladzo <vashchukvlad17@gmail.com>
2025-04-05 14:36:03 +0300

    Add relationship between Job and Employer

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Employer extends Model
 {
     use HasFactory;
+
+    public function jobs()
+    {
+        return $this->hasMany(Job::class);
+    }
 }
